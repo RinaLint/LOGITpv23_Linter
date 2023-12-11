735f6c4ace88c3ce2e950816dc735e51713b8257
opilane <opilane@HP-CZC2349HT2>
2023-12-11 11:23:20 +0200

    1-5 ülesanned

--- LOGITpv23_Linter.py
+++ LOGITpv23_Linter.py
@@ -1,3 +1,35 @@
+from random import *
+from math import *
+#4
+u=float(input("Ümbermõõt: ")) #L=pi*2*r=pi*d
+d=round(u/pi,2)
+print("Läbimõõt =",d)
+
+
+
+
+
+
+
+
+#3
+kokku=randint(1,100)
+print("Laual peal on",kokku,"kommid. Mitu tahad süüa?")
+kommid=int(input())
+kokku-=kommid
+print("Nüüd  kokku on",kokku)
+
+
+vanus=18
+eesnimi="Jaak"
+pikkus=16.5
+kas_käib_koolis=True
+
+print("Muutuja vanus=",vanus,"on", type(vanus))
+print("Muutuja eesnimi=",eesnimi,"on", type(eesnimi))
+print("Muutuja pikkus=",pikkus,"on", type(pikkus))
+print("Muutuja kas_käib_koolis=",kas_käib_koolis,"on", type(kas_käib_koolis))
+
 arv1=float(input("Arv1: "))
 t=input("Tehe: ")
 arv2=float(input("Arv2: "))
